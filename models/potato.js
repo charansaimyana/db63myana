028859310cad16ef5257425316c30a654c1ea277
Charan <s545263@nwmissouri.edu>
2021-12-02 00:01:23 -0600

    updated

--- models/potato.js
+++ models/potato.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose")
 const potatoPriceSchema = mongoose.Schema({
     type: String,
-    expdate: String,
-    cost: Number
+    expdate: {String,length=10},
+    cost: {
+        type: Number,
+        min: 0,
+        max: 7,
+    }
 })
 module.exports = mongoose.model("potato",
 potatoPriceSchema)
--- routes/potato.js
+++ routes/potato.js
@@ -1,6 +1,16 @@
 var express = require('express'); 
 const potato_controlers= require('../controllers/potato'); 
 var router = express.Router(); 
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
  
 
 /* GET costumes */
@@ -12,7 +22,7 @@ router.get('/detail', potato_controlers.potato_view_one_Page);
 router.get('/create', potato_controlers.potato_create_Page);
 
 
-router.get('/update', potato_controlers.potato_update_Page); 
+router.get('/update',secured, potato_controlers.potato_update_Page); 
 
 router.get('/delete', potato_controlers.potato_delete_Page); 
 
